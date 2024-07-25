405872f8d305700e2b96f7a867dffb1a3830c230
Victor Mazo <viamazoba@unal.edu.co>
2024-07-25 01:03:58 -0500

    feat: Se reliza conexión a la base de datos relacional.

---
+++ src/config/db.ts
@@ -0,0 +1,13 @@
+import { Sequelize } from 'sequelize'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const db = new Sequelize(process.env.DATABASE_URL!, {
+    dialectOptions: {
+        ssl: {
+            require: false
+        }
+    }
+})
+
+export default db
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,22 @@
 import express from "express";
 import router from "./routes";
+import db from './config/db'
+
+// COnectar a base de datos
+async function connectDB() {
+    try {
+
+        await db.authenticate()
+        db.sync()
+        console.log('Conexión exitosa a la BD')
+        
+    } catch (error) {
+        console.error(error)
+        console.log('Hubo un error al conectar la base de datos')
+    }
+}
+
+connectDB()
 
 const server = express()
 
